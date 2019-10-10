bfd9c7a9644f911ef6e0e1976a3ca8e9e4f63d2e
Shymaxtic <shymaxtic@gmail.com>
2019-10-10 21:10:35 +0700

    add file

---
+++ main.c
@@ -0,0 +1,158 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
+#include "driverlib/rom.h"
+#include "grlib/grlib.h"
+#include "usblib/usblib.h"
+#include "usblib/usb-ids.h"
+#include "usblib/device/usbdevice.h"
+#include "usblib/device/usbdbulk.h"
+#include "utils/uartstdio.h"
+#include "utils/ustdlib.h"
+#include <string.h>
+
+#define DEBUG
+//*****************************************************************************
+//
+// The system tick rate expressed both as ticks per second and a millisecond
+// period.
+//
+//*****************************************************************************
+#define SYSTICKS_PER_SECOND 100
+#define SYSTICK_PERIOD_MS (1000 / SYSTICKS_PER_SECOND)
+
+//*****************************************************************************
+//
+// The global system tick counter.
+//
+//*****************************************************************************
+volatile uint32_t g_ui32SysTickCount = 0;
+
+#ifdef DEBUG
+//*****************************************************************************
+//
+// Map all debug print calls to UARTprintf in debug builds.
+//
+//*****************************************************************************
+#define DEBUG_PRINT UARTprintf
+
+#else
+//#define DEBUG_PRINT while(0) ((int32_t (*)(char *, ...))0)
+#endif
+
+//*****************************************************************************
+//
+// The error routine that is called if the driver library encounters an error.
+//
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+    UARTprintf("Error at line %d of %s\n", ui32Line, pcFilename);
+    while(1)
+    {
+    }
+}
+#endif
+
+//*****************************************************************************
+//
+// Interrupt handler for the system tick counter.
+//
+//*****************************************************************************
+void
+SysTickIntHandler(void)
+{
+    //
+    // Update our system tick counter.
+    //
+    g_ui32SysTickCount++;
+}
+
+#ifdef DEBUG
+//*****************************************************************************
+//
+// Configure the UART and its pins.  This must be called before UARTprintf().
+//
+//*****************************************************************************
+void
+ConfigureUART(void)
+{
+    //
+    // Enable the GPIO Peripheral used by the UART.
+    //
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    //
+    // Enable UART0
+    //
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    //
+    // Configure GPIO Pins for UART mode.
+    //
+    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    //
+    // Use the internal 16MHz oscillator as the UART clock source.
+    //
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    //
+    // Initialize the UART for console I/O.
+    //
+    UARTStdioConfig(0, 115200, 16000000);
+    //
+    UARTStdioConfig(0, 115200, 16000000);
+}
+#endif
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    //
+    // Enable lazy stacking for interrupt handlers.  This allows floating-point
+    // instructions to be used within interrupt handlers, but at the expense of
+    // extra stack usage.
+    //
+//    ROM_FPULazyStackingEnable();
+
+    //
+    // Set the clocking to run from the PLL at 50MHz
+    //
+//    ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+//                       SYSCTL_XTAL_16MHZ);
+
+#ifdef DEBUG
+    //
+    // Configure the UART for debug output.
+    //
+    ConfigureUART();
+#endif
+
+    // Enable the system tick.
+//    ROM_SysTickPeriodSet(ROM_SysCtlClockGet() / SYSTICKS_PER_SECOND);
+//    ROM_SysTickIntEnable();
+//    ROM_SysTickEnable();
+
+//    DEBUG_PRINT("\nTiva C Series USB bulk device example\n");
+//    DEBUG_PRINT("---------------------------------\n\n");
+
+    while(1) {}
+}
